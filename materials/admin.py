be39d2449180cbeadad8fd640040dfdc9ce9b0ba
janmdvoracek <jan.m.dvoracek@gmail.com>
2026-08-08 11:33:57 +0200

    Added functionality to the admin panel

--- materials/admin.py
+++ materials/admin.py
@@ -6,6 +6,7 @@ from .models import Location, Machine, Material
 @admin.register(Material)
 class MaterialAdmin(admin.ModelAdmin):
     list_display = ("name", "sku", "unit_of_measure", "category", "track_stock", "is_active")
+    list_editable = ("category", "track_stock", "is_active")
     list_filter = ("category", "track_stock", "is_active")
     search_fields = ("name", "sku")
 
@@ -13,11 +14,12 @@ class MaterialAdmin(admin.ModelAdmin):
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ("name", "is_active")
+    list_editable = ("is_active",)
 
 
 @admin.register(Machine)
 class MachineAdmin(admin.ModelAdmin):
     list_display = ("name", "total_hours", "hourly_rate", "is_active")
+    list_editable = ("total_hours", "hourly_rate", "is_active")
     list_filter = ("is_active",)
     search_fields = ("name",)
-    readonly_fields = ("total_hours",)
